05624cdba3a02e664646424aa5c240e4163dbe1c
waz <wazwaztime@gmail.com>
2026-02-02 14:01:41 +0800

    modify node.h

--- include/node.h
+++ include/node.h
@@ -18,7 +18,6 @@ class Node;
 
 template <typename T>
 using NodePtr = std::shared_ptr<Node<T>>;
-template <typename T>
 
 /*! \brief node class */
 template <typename T>
@@ -30,6 +29,11 @@ class Node : public std::enable_shared_from_this<Node<T>> {
   std::string name_;
 
  public:
+  explicit Node(const std::vector<NodePtr<T>>& inputs,
+       const std::vector<NodePtr<T>>& outputs,
+       const std::string& name)
+    : inputs_(inputs), outputs_(outputs), name_(name) {}
+  Node() = default;
   virtual ~Node() = default;
 
   /*! \brief add an input node */
@@ -64,24 +68,20 @@ class Node : public std::enable_shared_from_this<Node<T>> {
 };
 
 /*! \brief data node class */
-template <typename T, typename Deleter = std::default_delete<T[]>>
+template <typename T>
 class DataNode : public Node<T> {
  public:
+  /*! \brief get the pointer to the data */
+  virtual T* data() = 0;
+
+  /*! \brief get the pointer to the gradient */
+  virtual T* grad() = 0;
+
   /*! \brief get the shape of the data */
   const std::vector<size_t>& shape() const {
     return shape_;
   }
 
-  /*! \brief get the pointer to the data */
-  T* data() {
-    return data_.get();
-  }
-
-  /*! \brief get the pointer to the gradient */
-  T* grad() {
-    return grad_.get();
-  }
-  
   /*! \brief set whether the node requires gradient */
   void set_requires_grad(bool requires_grad) {
     requires_grad_ = requires_grad;
@@ -94,18 +94,26 @@ class DataNode : public Node<T> {
 
  private:
   std::vector<size_t> shape_; 
-  std::unique_ptr<T[], Deleter> data_;
-  std::unique_ptr<T[], Deleter> grad_;
 };
 
 /*! \brief compute node class */
 template <typename T>
 class ComputeNode : public Node<T> {
  public:
-  ComputeNode(const std::vector<NodePtr<T>>& inputs,
+  explicit ComputeNode(const std::vector<NodePtr<T>>& inputs,
              const std::vector<NodePtr<T>>& outputs,
              const std::string& name = "") 
-   : inputs_(inputs), outputs_(outputs), name_(name) {
+   : Node<T>(inputs, outputs, name) {
+    // Assert that all inputs are DataNode
+    for (const auto& input : inputs_) {
+      assert(std::dynamic_pointer_cast<DataNode<T>>(input) != nullptr && 
+             "ComputeNode inputs must be DataNode instances");
+    }
+    // Assert that all outputs are DataNode
+    for (const auto& output : outputs_) {
+      assert(std::dynamic_pointer_cast<DataNode<T>>(output) != nullptr && 
+             "ComputeNode outputs must be DataNode instances");
+    }
     for (const auto& input : inputs_) {
       input->add_output(this->shared_from_this());
     }
@@ -119,7 +127,7 @@ class ComputeNode : public Node<T> {
 
   ~ComputeNode() override = default;
 
-  /*! \brief assert the shape of the input and output*/
+  /*! \brief validate the shape of the input and output*/
   virtual bool validate_shape() = 0;
 
   /*! \brief forward computation */
@@ -129,50 +137,6 @@ class ComputeNode : public Node<T> {
   virtual void backward() = 0;
   
 };
-
-/*! \brief addition node class */
-template <typename T>
-class AddNode : public ComputeNode<T> {
- public:
-  bool validate_shape() override {
-    if (this->inputs_.size() != 2 || this->outputs_.size() != 1) {
-      return false;
-    }
-    if (dynamic_cast<DataNode<T>*>(this->inputs_[0].get()) == nullptr ||
-        dynamic_cast<DataNode<T>*>(this->inputs_[1].get()) == nullptr ||
-        dynamic_cast<DataNode<T>*>(this->outputs_[0].get()) == nullptr) {
-      return false;
-    }
-    auto shape1 = this->inputs_[0]->shape();
-    auto shape2 = this->inputs_[1]->shape();
-    auto out_shape = this->outputs_[0]->shape();
-    return shape1 == shape2 && shape1 == out_shape;
-  }
-
-  void forward() override {
-    if (!assertShape()) {
-      throw std::runtime_error("Shape mismatch in AddNode forward");
-    }
-    for (size_t i = 0; i < this->inputs_[0]->shape()[0]; ++i) {
-      this->outputs_[0]->data()[i] = this->inputs_[0]->data()[i] + this->inputs_[1]->data()[i];
-    }
-  }
-
-  void backward() override {
-    if (!assertShape()) {
-      throw std::runtime_error("Shape mismatch in AddNode backward");
-    }
-    for (size_t i = 0; i < this->inputs_[0]->shape()[0]; ++i) {
-      if (this->inputs_[0]->requires_grad()) {
-        this->inputs_[0]->grad()[i] += this->outputs_[0]->grad()[i];
-      }
-      if (this->inputs_[1]->requires_grad()) {
-        this->inputs_[1]->grad()[i] += this->outputs_[0]->grad()[i];
-      }
-    }
-  }
-};
-
 
 } // namespace modeldy
 
